7c059b827e44f3653e5e5fbf46affe6f7db7844c
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-09-30 16:00:11 +0300

    [MODIFIED] added spaces

--- ImageSharpening/ImageSharpening.m
+++ ImageSharpening/ImageSharpening.m
@@ -5,8 +5,8 @@
 % Read the mri image
 close all
 clear all
-img=imread('290px-MRI_Head_Brain_Normal.jpg');
-im_mri=im2double(img);
+img = imread('290px-MRI_Head_Brain_Normal.jpg');
+im_mri = im2double(img);
 
 % =========================================================================
 % PART 1: Sobel and Prewitt
@@ -18,26 +18,26 @@ input_mr_prewitt = prewitt_filter(im_mri);
 
 % Print and save figures
 figure (1)
-imshow(input_mr_sobel,[]);
+imshow(input_mr_sobel, []);
 xlabel('(\alpha) After Sobel filter') 
-imwrite(input_mr_sobel/max(max(input_mr_sobel)),'MR figure11.jpg');
+imwrite(input_mr_sobel/max(max(input_mr_sobel)), 'MR figure11.jpg');
 figure (2)
-imshow(input_mr_prewitt,[]);
+imshow(input_mr_prewitt, []);
 xlabel('(\alpha) After Prewitt filter') 
-imwrite(input_mr_prewitt/max(max(input_mr_prewitt)),'MR figure12.jpg');
+imwrite(input_mr_prewitt/max(max(input_mr_prewitt)), 'MR figure12.jpg');
 
 % =========================================================================
 % PART 2: Verification
 % =========================================================================
 
-[Gmag_mr_sobel, Gdir_mr_sobel] = imgradient(im_mri,'sobel');
+[Gmag_mr_sobel, Gdir_mr_sobel] = imgradient(im_mri, 'sobel');
 disp('Is the MR image calculated by the sobel_filter() the same as the MR image produced by the imgradient() matlab function?')
 if(max(max(abs(Gmag_mr_sobel - input_mr_sobel))) < 1e-15)
     disp('Yes')
 else
     disp('No')
 end
-[Gmag_mr_prewitt, Gdir_mr_prewitt] = imgradient(im_mri,'prewitt');
+[Gmag_mr_prewitt, Gdir_mr_prewitt] = imgradient(im_mri, 'prewitt');
 disp('Is the MR image calculated by the prewitt_filter() the same as the MR image produced by the imgradient() matlab function?')
 if(max(max(abs(Gmag_mr_prewitt - input_mr_prewitt))) < 1e-15)
     disp('Yes')
