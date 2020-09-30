135f80704737f7f18641b70ac57d71eb605ecfb2
Dimitra Kaitalidou <49205589+DimitraKaitalidou@users.noreply.github.com>
2020-09-30 15:56:04 +0300

    [MODIFIED] added spaces

--- ImageSharpening/prewitt_filter.m
+++ ImageSharpening/prewitt_filter.m
@@ -8,7 +8,7 @@ function[output] = prewitt_filter(input)
 r_pad = r + 2;
 c_pad = c + 2;
 input_prewitt = zeros(r_pad, c_pad);
-input_pad = padarray(input,[1 1],'replicate','both');
+input_pad = padarray(input, [1 1], 'replicate', 'both');
 total1 = 0;
 total2 = 0;
 
@@ -16,22 +16,22 @@ gx = [-1 0 1; -1 0 1; -1 0 1];
 gy = [-1 -1 -1; 0 0 0; 1 1 1];
 
 % Apply the prewitt filter
-for j=2:r_pad-1
-    for i=2:c_pad-1
-        for h=-1:1
-            for g=-1:1
-                total1 = total1+input_pad(j+h,i+g)*gx(h+2,g+2);
-                total2 = total2+input_pad(j+h,i+g)*gy(h+2,g+2);
+for j = 2:r_pad - 1
+    for i = 2:c_pad - 1
+        for h = -1:1
+            for g = -1:1
+                total1 = total1 + input_pad(j + h, i + g) * gx(h + 2, g + 2);
+                total2 = total2 + input_pad(j + h, i + g) * gy(h + 2, g + 2);
             end
         end
-        input_prewitt(j,i) = sqrt(total1*total1+total2*total2);
+        input_prewitt(j,i) = sqrt(total1 * total1 + total2 * total2);
         total1 = 0;
         total2 = 0;
     end 
 end
 
 % Populate the output matrix
-output = input_prewitt(2:r_pad-1, 2:c_pad-1);
+output = input_prewitt(2:r_pad - 1, 2:c_pad - 1);
 
 end
 
